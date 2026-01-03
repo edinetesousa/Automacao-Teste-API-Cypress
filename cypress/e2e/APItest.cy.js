f37947fe5eeb31e2e13af09e81dd14153ef024c7
Edinete Sousa <edineteb.sousa@gmail.com>
2026-01-03 18:14:39 -0300

    adicionando o cypress-plugin-api

--- cypress/e2e/APItest.cy.js
+++ cypress/e2e/APItest.cy.js
@@ -1,42 +1,42 @@
-const { expect } = require("chai")
-
 describe('API de Severest', () => {
 
   it('Criar usuario', () => {
-    cy.request('POST', 'https://serverest.dev/usuarios', {
-      "nome": "João da Silva Bel",
-      "email": "joaodasilvabel@qa.com.br",
-      "password": "teste",
-      "administrador": "true"
-    }
-    ).then((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body).to.have.property('message', 'Cadastro realizado com sucesso')
-      Cypress.env('Id', response.body._id)
-    })
+    cy.cria_user()
   })
+
   it('Listar usuarios', () => {
-    cy.request('GET', 'https://serverest.dev/usuarios')
+    cy.api('GET', 'https://serverest.dev/usuarios')
       .then((response) => {
         expect(response.status).to.eq(200)
       })
   })
+
   it('Buscar usuario por ID', () => {
-    cy.request('GET', `https://serverest.dev/usuarios/${Cypress.env('Id')}`)
+    cy.api('GET', `https://serverest.dev/usuarios/${Cypress.env('Id')}`)
       .then((response) => {
         expect(response.status).to.eq(200)
       })
   })
 
   it('Atualiza usuario', () => {
-    cy.request('PUT', `https://serverest.dev/usuarios/${Cypress.env('Id')}`, {
-      "nome": "João Luiz da Silva Beltrano",
-      "email": "joaoluizdasilvabel@qa.com.br",
+    cy.api('PUT', `https://serverest.dev/usuarios/${Cypress.env('Id')}`, {
+      "nome": "João beleu",
+      "email": "joaobeleu@qa.com.br",
       "password": "teste",
       "administrador": "true"
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.message).to.eq('Registro alterado com sucesso')
     })
-  }).then((response) => {
-    expect(response.status).to.eq(200)
-    expect(response.body.message).to.eq('Registro alterado com sucesso')
+  })
+
+
+  it('Excluir usuario', () => {
+    cy.api('DELETE', `https://serverest.dev/usuarios/${Cypress.env('Id')}`
+
+    ).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.message).to.eq('Registro excluído com sucesso')
+    })
   })
 })
